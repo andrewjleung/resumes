a9449a0186fba4885f598684194ac72112b34209
Andrew Leung <andrewleung104@gmail.com>
2024-09-29 15:30:38 -0500

    Remove anonymous resume
    This was too annoying to maintain, I'll revisit it and find a way to more easily maintain an anonymized resume (automatic anonymization?).

--- render.ts
+++ render.ts
@@ -1,5 +1,4 @@
 import resume from './resume';
-import anonymousResume from './resume.anonymous';
 import latex from 'node-latex';
 import fs from 'fs';
 import { resume as r, BareResume } from './resumeBuilder';
@@ -10,8 +9,6 @@ const SECOND_COOP_START_DATE = new Date('2020-01-06');
 const ARTIFACTS_PATH = './artifacts';
 const RESUME_TEX = 'main.tex';
 const RESUME_PDF = 'AndrewLeung_Resume.pdf';
-const ANONYMOUS_RESUME_TEX = 'anonymous.tex';
-const ANONYMOUS_RESUME_PDF = 'Anonymous_Resume.pdf';
 
 const artifact = (filename: string): string => `${ARTIFACTS_PATH}/${filename}`;
 
@@ -60,8 +57,3 @@ const renderAndWriteResume = (
 };
 
 renderAndWriteResume(resume, artifact(RESUME_TEX), artifact(RESUME_PDF));
-renderAndWriteResume(
-  anonymousResume,
-  artifact(ANONYMOUS_RESUME_TEX),
-  artifact(ANONYMOUS_RESUME_PDF),
-);
