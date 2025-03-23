31e7f73092b5cabe6c37a6d4ec4f84b34e7c7cba
Andrew Leung <andrewleung104@gmail.com>
2025-03-23 17:41:35 -0500

    Convert into CLI

--- render.ts
+++ render.ts
@@ -2,7 +2,7 @@ import resume from './resume';
 import latex from 'node-latex';
 import fs from 'fs';
 import { resume as r } from './resumeRenderer';
-import { BareResume } from './types';
+import { program } from 'commander';
 
 const SECOND_COOP_START_DATE = new Date('2020-01-06');
 
@@ -12,16 +12,20 @@ const RESUME_PDF = 'AndrewLeung_Resume.pdf';
 
 const artifact = (filename: string): string => `${ARTIFACTS_PATH}/${filename}`;
 
+const writeResumeTex = (renderedResume: string, texFileName: string) => {
+  fs.writeFileSync(texFileName, renderedResume, {
+    encoding: 'utf-8',
+    flag: 'w',
+  });
+};
+
 const writeResume = (
   renderedResume: string,
   texFileName: string,
   pdfFileName: string,
 ) => {
   // Write to an intermediate LaTeX file before generating PDF for debugging.
-  fs.writeFileSync(texFileName, renderedResume, {
-    encoding: 'utf-8',
-    flag: 'w',
-  });
+  writeResumeTex(renderedResume, texFileName);
 
   // Update PDF.
   const input = fs.createReadStream(texFileName);
@@ -33,27 +37,29 @@ const writeResume = (
   pdf.on('finish', () => console.log(`${pdfFileName} updated!`));
 };
 
-const renderAndWriteResume = (
-  resume: BareResume,
-  texFileName: string,
-  pdfFileName: string,
-): void => {
-  const renderedResume = r(resume)
-    .experiences({
-      after: new Date(SECOND_COOP_START_DATE),
-      exclude: ['Sandbox at Northeastern University'],
-    })
-    .projects({
-      include: ['Bookmark Manager', 'Compiler for Python-like Language'],
-    })
-    .render({ lineHeight: 1.2, margin: 0.75 }, [
-      'skills',
-      'experiences',
-      'projects',
-      'education',
-    ]);
+program.option('-t, --tex', 'only create a .tex file');
+program.parse(process.argv);
 
-  writeResume(renderedResume, texFileName, pdfFileName);
-};
+const options = program.opts();
+const renderedResume = r(resume)
+  .experiences({
+    after: new Date(SECOND_COOP_START_DATE),
+    exclude: ['Sandbox at Northeastern University'],
+  })
+  .projects({
+    include: ['Bookmark Manager', 'Compiler for Python-like Language'],
+  })
+  .render({ lineHeight: 1.2, margin: 0.75 }, [
+    'skills',
+    'experiences',
+    'projects',
+    'education',
+  ]);
 
-renderAndWriteResume(resume, artifact(RESUME_TEX), artifact(RESUME_PDF));
+console.log(options);
+
+if (options.tex) {
+  writeResumeTex(renderedResume, artifact(RESUME_TEX));
+} else {
+  writeResume(renderedResume, artifact(RESUME_TEX), artifact(RESUME_PDF));
+}
