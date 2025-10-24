25769cc611c703272685780a0ad0f7d6c857e31f
Andrew Leung <andrewleung104@gmail.com>
2025-10-24 15:27:21 -0500

    Reload config on watch refresh
    Signed-off-by: Andrew Leung <andrewleung104@gmail.com>

--- src/world.rs
+++ src/world.rs
@@ -1,13 +1,10 @@
 use camino::Utf8PathBuf as PathBuf;
 use std::iter::once;
 
-use crate::config::Config;
-
 #[derive(Clone)]
 pub struct World {
     pub artifact_title: String,
     pub clean: bool,
-    pub config: Option<Config>,
     pub extra_watched_file_paths: Vec<PathBuf>,
     pub output_dir: PathBuf,
     pub resume_data_path: PathBuf,
