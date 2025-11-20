ebb58b9a0eab121aec34a775bb62deac8558d0ae
Andrew Leung <andrewleung104@gmail.com>
2025-11-20 15:54:54 -0500

    Support local configs
    We'll first walk through all the ancestors of the current working
    directory (including the current working directory) and look for the
    nearest file called "reze.toml".
    
    If no such file is found, we'll check the global config path,
    ".config/reze/reze.toml", for a config.
    
    If neither is found we give up.
    
    Currently we do not check that file is well-formed when walking up the
    CWD ancestors, so in the situation that the nearest config is malformed
    but there is another farther well-formed config, we won't return that
    farther config.
    
    Signed-off-by: Andrew Leung <andrewleung104@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,10 @@
+use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
 use std::io::read_to_string;
 
-use anyhow::{Context, Result};
+use anyhow::{Context, Result, anyhow};
 use camino::Utf8Path as Path;
 use camino::Utf8PathBuf as PathBuf;
 use serde::Deserialize;
@@ -28,22 +29,47 @@ pub struct Config {
     pub projects: Option<ProjectConfig>,
 }
 
-pub fn default_config_path() -> PathBuf {
-    dirs::home_dir()
-        .map(|dir| {
-            PathBuf::from_path_buf(dir.join(".config/reze/reze.toml"))
-                .expect("config path should be UTF-8")
-        })
-        .expect("home directory should exist")
+const CONFIG_FILE_NAME: &str = "reze.toml";
+const DEFAULT_CONFIG_SUBDIRECTORY: &str = "reze";
+
+pub fn default_config_path() -> Result<PathBuf> {
+    let home_dir = dirs::home_dir().context("failed to get path to home directory")?;
+    let path = home_dir
+        .join(".config")
+        .join(DEFAULT_CONFIG_SUBDIRECTORY)
+        .join(CONFIG_FILE_NAME);
+
+    try_into_utf8pathbuf(&path)
 }
 
-#[allow(dead_code)]
-fn tilde_expand(dir: &Path) -> PathBuf {
-    Path::new(&shellexpand::tilde(dir.as_str())).to_path_buf()
+fn try_into_utf8pathbuf(path: &std::path::Path) -> Result<PathBuf> {
+    PathBuf::from_path_buf(path.to_path_buf()).map_err(|_| {
+        anyhow!(
+            "failed to parse path into UTF-8: {}",
+            path.to_string_lossy()
+        )
+    })
+}
+
+fn find_in_ancestors(path: &Path, file_name: &str) -> Option<PathBuf> {
+    for ancestor in path.ancestors() {
+        if let Ok(entries) = ancestor.read_dir() {
+            let target = entries
+                .filter_map(|entry| entry.ok())
+                .find(|entry| entry.path().is_file() && entry.file_name() == file_name)
+                .and_then(|target| try_into_utf8pathbuf(&target.path()).ok());
+
+            if target.is_some() {
+                return target;
+            }
+        }
+    }
+
+    None
 }
 
 pub fn init() -> Result<bool> {
-    let config_path = default_config_path();
+    let config_path = default_config_path()?;
 
     if fs::exists(&config_path).context("failed to check for existence of config path")? {
         return Ok(false);
@@ -66,18 +92,22 @@ pub fn init() -> Result<bool> {
 }
 
 pub fn config() -> Result<Option<Config>> {
-    let config_path = default_config_path();
+    let cwd = env::current_dir()
+        .context("failed to retrieve current working directory")
+        .and_then(|path| try_into_utf8pathbuf(&path))
+        .ok(); // TODO: logging?
 
-    if !fs::exists(&config_path).context("failed to check for existence of config path")? {
-        return Ok(None);
-    }
-
-    Ok(Some(
-        File::open(config_path)
+    let ancestor_config_path = cwd.and_then(|dir| find_in_ancestors(&dir, CONFIG_FILE_NAME));
+    let config_path = ancestor_config_path.or_else(|| default_config_path().ok());
+    let config = config_path.and_then(|path| {
+        File::open(path)
             .context("failed to open config")
             .and_then(|file| read_to_string(file).context("failed to read config"))
             .and_then(|content| {
                 toml::from_str(&content).context("failed to parse config into TOML")
-            })?,
-    ))
+            })
+            .ok() // TODO: logging?
+    });
+
+    Ok(config)
 }
